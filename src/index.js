d64e44d8db2745c49f41e1c159d8ddb02803b972
Carlos Jesus Bermudez <82308312+Carlosjbermu10@users.noreply.github.com>
2024-05-16 03:29:35 -0400

    volvimos con todo

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-import  express  from 'express';
-import morgan from 'morgan';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import express from "express";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 //importamos las variables de entorno
-import { PORT } from './config.js';
+import { PORT } from "./config.js";
 
-//importamos las rutas 
-import loginRoutes from './routes/users/login.routes.js'
-import RegisterRoutes from './routes/users/register.routes.js'
-import categoryRoutes from './routes/category.routes.js';
-import imag_categoryRoutes from './routes/imag/imag_category.routes.js';
-import productRoutes from './routes/product.routes.js';
-import imag_productRoutes from './routes/imag/imag_product.routes.js';
-import carRoutes from './routes/car.routes.js';
-import buyRoutes from './routes/buy.routes.js';
+//importamos las rutas
+import loginRoutes from "./routes/users/login.routes.js";
+import RegisterRoutes from "./routes/users/register.routes.js";
+import categoryRoutes from "./routes/category.routes.js";
+import imag_categoryRoutes from "./routes/imag/imag_category.routes.js";
+import productRoutes from "./routes/product.routes.js";
+import imag_productRoutes from "./routes/imag/imag_product.routes.js";
+import carRoutes from "./routes/car.routes.js";
+import buyRoutes from "./routes/buy.routes.js";
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 
 //para procesar datos enviados desde el form
-app.use(express.urlencoded({extended:false}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 //inicializar morgan
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
 //para poder trabajar con las cookies
-app.use(cookieParser())
+app.use(cookieParser());
 
 // Para eliminar el cache y que no se pueda volver con el boton de back luego que hagamos un LOGOUT
 /*app.use((req, res, next) => {
@@ -36,15 +36,15 @@ app.use(cookieParser())
     }
 })*/
 
-//llamar al Router de login 
-app.use(loginRoutes)
-app.use(RegisterRoutes) 
-app.use(categoryRoutes) 
-app.use(imag_categoryRoutes) 
-app.use(productRoutes)
-app.use(imag_productRoutes)
-app.use(carRoutes)
-app.use(buyRoutes)
+//llamar al Router de login
+app.use(loginRoutes);
+app.use(RegisterRoutes);
+app.use(categoryRoutes);
+app.use(imag_categoryRoutes);
+app.use(productRoutes);
+app.use(imag_productRoutes);
+app.use(carRoutes);
+app.use(buyRoutes);
 
-app.listen(PORT)
-console.log('server running in the port', PORT)
+app.listen(PORT);
+console.log("Server running in the port", PORT);
