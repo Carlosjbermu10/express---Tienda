f1013ee73216dcb6d3a1e9c77366eba79ea62c2d
Carlos Jesus Bermudez <82308312+Carlosjbermu10@users.noreply.github.com>
2024-05-28 02:11:54 -0400

    se corrigio entre comillas todo ajsjas

--- src/controllers/buy.controller.js
+++ src/controllers/buy.controller.js
@@ -1,129 +1,131 @@
-import { VerifyToken } from '../helpers/GenerateToken.js';
+import { VerifyToken } from "../helpers/GenerateToken.js";
 
-import { paypal } from '../helpers/paypal.js';
+import { paypal } from "../helpers/paypal.js";
 
-import { getAllBuyForUser, //Servicio que devuelve todas las compras del usuario que inicio seccion
-    SearchBuyId, //Servicio que busca si ya existe una Compra por el Id
-    getDetailBuy, //Servicio que devuelve los detalles de una compra
-    SearchPayId, //Servicio que busca si ya existe un tipo de pago por el Id
-    SearchUserId, //Servicio que busca si ya existe un usuario por el Id
-    Total_Buy, //Servicio que devuelve el monto total a pagar en una compra
-    RegisterBuy, //Servicio que registra una compra
-    Data_Products, //Servicio que devuelve los datos de los productos que se estan comprando
-    deletecar, //Servicio que deletea el carrito de un usuario luego de la compra
-    RegisterDetail_buy //Servicio que registra los detalles de una compra
-    } from '../services/buy.services.js';
+import {
+  getAllBuyForUser, //Servicio que devuelve todas las compras del usuario que inicio seccion
+  SearchBuyId, //Servicio que busca si ya existe una Compra por el Id
+  getDetailBuy, //Servicio que devuelve los detalles de una compra
+  SearchPayId, //Servicio que busca si ya existe un tipo de pago por el Id
+  SearchUserId, //Servicio que busca si ya existe un usuario por el Id
+  Total_Buy, //Servicio que devuelve el monto total a pagar en una compra
+  RegisterBuy, //Servicio que registra una compra
+  Data_Products, //Servicio que devuelve los datos de los productos que se estan comprando
+  deletecar, //Servicio que deletea el carrito de un usuario luego de la compra
+  RegisterDetail_buy, //Servicio que registra los detalles de una compra
+} from "../services/buy.services.js";
 
-export const getBuy = async (req,res) => {
+export const getBuy = async (req, res) => {
+  try {
+    //Se busca el id del usuario que inicio seccion por el token que esta en la cookie
+    const decodificada = await VerifyToken(req);
 
-    try {
-
-        //Se invoca a el metodo que devuelve todas las compras de el usuario registrado
-        const allbuyUser = await getAllBuyForUser()
-
-        res.send({  status:"ok",
-                    description:"Lista de Compras",
-                    data:allbuyUser})
-
-    } catch (error) {
-        
+    //Se comprueba si ya existe el usuario
+    const search_user = await SearchUserId(decodificada.id);
+    if (search_user == 0) {
+      return res.send({ status: "mal", description: "Usuario No registrado" });
     }
-    
-}
 
-export const getDetail_Buy = async (req,res) => {
+    //Se invoca a el metodo que devuelve todas las compras de el usuario registrado
+    const allbuyUser = await getAllBuyForUser(decodificada.id);
 
-    try {
-
-        // se reciben la variable que viene por parametros
-        const id_buy = req.params.id
-        //Se comprueba si ya existe esa compra
-        const search_buy = await SearchBuyId(id_buy)
-        if (search_buy == 0){
-            return res.send({ status:"mal",
-            description:"Compra no registrada",
-            })
-        }
-
-        //Se invoca a el metodo que devuelve todas los detalles de una compra
-        const detail_buy = await getDetailBuy()
-
-        res.send({  status:"ok",
-                    description:"Detalles de compra",
-                    data:detail_buy})
-
-    } catch (error) {
-        
+    if (allbuyUser == 0) {
+      res.send({
+        status: "ok",
+        description: "La lista de compra se encuentra vacia",
+      });
+    } else {
+      res.send({
+        status: "ok",
+        description: "Lista de Compras",
+        data: allbuyUser,
+      });
     }
-    
-}
+  } catch (error) {}
+};
 
+export const getDetail_Buy = async (req, res) => {
+  try {
+    // se reciben la variable que viene por parametros
+    const id_buy = req.params.id;
 
-export const postBuy = async (req,res) => {
-
-    try {
-        
-        // se reciben la variable que viene por parametros
-        const id_pay = req.params.id
-
-        //Se comprueba si ya existe ese pago
-        const search_pay = await SearchPayId(id_pay)
-        if (search_pay == 0){
-            return res.send({ status:"mal",
-            description:"Metodo de pago no registrado",
-            })
-        }
-
-        //Se busca el id del usuario que inicio seccion por el token que esta en la cookie
-        const decodificada = await VerifyToken(req)
-
-        //Se comprueba si ya existe el usuario
-        const search_user = await SearchUserId(decodificada.id)
-        if (search_user == 0){
-            return res.send({ status:"mal",
-            description:"usuario No registrado",
-            })
-        }
-
-        //Se invoca el metodo que devuelve el total de los productos que estan en el carrito
-        const total = await Total_Buy(decodificada.id)
-
-        console.log(total)
-        
-        //Se crea un objeto para pasarlo mas adelante
-        const bu = {
-            total: total.total,
-            id_users: decodificada.id,
-            id_pay: id_pay
-        };
-
-        //se invoca el servicio para registrar la compra de un usuario
-        const buy = await RegisterBuy(bu)
-
-        //Se invoca el servicio que devuelve los productos que se estan comprando
-        let produ_buy = await Data_Products(decodificada.id)
-        const all_produ = produ_buy.map(element => {
-            return{ 
-                id_buys: buy.id,
-                id_product: element.id_product,
-                name: element.name_product,
-                price: element.price_product,
-                amount: element.amount_car
-            }
-        })
-
-        //Se invoca el metodo que deletea el carito de compra del usuario
-        const del_car = await deletecar(decodificada.id)
-        
-        //Se invoca el servicio que registra los detalles de una compra
-        const detail_buy = await  RegisterDetail_buy(all_produ)
-
-        res.send({  status:"ok",
-                    description:"Compra registrada correctamente",
-                    data:buy})
-        
-    } catch (error) {
-        console.log(error)
+    //Se comprueba si ya existe esa compra
+    const search_buy = await SearchBuyId(id_buy);
+    if (search_buy == 0) {
+      return res.send({ status: "mal", description: "Compra no registrada" });
     }
-    
-}
+
+    //Se invoca a el metodo que devuelve todas los detalles de una compra
+    const detail_buy = await getDetailBuy();
+
+    res.send({
+      status: "ok",
+      description: "Detalles de compra",
+      data: detail_buy,
+    });
+  } catch (error) {}
+};
+
+export const postBuy = async (req, res) => {
+  try {
+    // se reciben la variable que viene por parametros
+    const id_pay = req.params.id;
+
+    //Se comprueba si ya existe ese pago
+    const search_pay = await SearchPayId(id_pay);
+    if (search_pay == 0) {
+      return res.send({
+        status: "mal",
+        description: "Metodo de pago no registrado",
+      });
+    }
+
+    //Se busca el id del usuario que inicio seccion por el token que esta en la cookie
+    const decodificada = await VerifyToken(req);
+
+    //Se comprueba si ya existe el usuario
+    const search_user = await SearchUserId(decodificada.id);
+    if (search_user == 0) {
+      return res.send({ status: "mal", description: "usuario No registrado" });
+    }
+
+    //Se invoca el metodo que devuelve el total de los productos que estan en el carrito
+    const total = await Total_Buy(decodificada.id);
+
+    //Se crea un objeto para pasarlo mas adelante
+    const bu = {
+      total: total.total,
+      id_users: decodificada.id,
+      id_pay: id_pay,
+    };
+
+    //se invoca el servicio para registrar la compra de un usuario
+    const buy = await RegisterBuy(bu);
+
+    //Se invoca el servicio que devuelve los productos que se estan comprando
+    let produ_buy = await Data_Products(decodificada.id);
+    const all_produ = produ_buy.map((element) => {
+      return {
+        id_buys: buy.id,
+        id_product: element.id_product,
+        name: element.name_product,
+        price: element.price_product,
+        amount: element.amount_car,
+      };
+    });
+
+    //Se invoca el metodo que deletea el carito de compra del usuario
+    const del_car = await deletecar(decodificada.id);
+
+    //Se invoca el servicio que registra los detalles de una compra
+    const detail_buy = await RegisterDetail_buy(all_produ);
+
+    res.send({
+      status: "ok",
+      description: "Compra registrada correctamente",
+      data: buy,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/services/buy.services.js
+++ src/services/buy.services.js
@@ -1,132 +1,125 @@
-import { pool } from '../database/db.js';
+import { pool } from "../database/db.js";
 
 //Servicio que devuelve todas las compras del usuario que inicio seccion
-export const getAllBuyForUser = async() => {
-    
-    const [rows] = await pool.query(`select 
+export const getAllBuyForUser = async (id_users) => {
+  const [rows] = await pool.query(
+    `select 
     b.id_buy, b.total, b.date, pa.name_pay
     from users as u
     inner join buys as b
-    on u.id_users = b.id_users
+    on u.id_users = ?
     inner join pay as pa
-    on b.id_pay = pa.id_pay; `)
-    return rows
-
-}
+    on b.id_pay = pa.id_pay; `,
+    [id_users]
+  );
+  return rows;
+};
 
 //Servicio que busca si ya existe una Compra por el Id
 export const SearchBuyId = async (id) => {
-
-    const [rows] = await pool.query('SELECT * FROM buys WHERE id_buy = ?', [id])
-    return rows.length
-    
-}
+  const [rows] = await pool.query("SELECT * FROM buys WHERE id_buy = ?", [id]);
+  return rows.length;
+};
 
 //Servicio que devuelve los detalles de una compra
-export const getDetailBuy = async() => {
-    
-    const [rows] = await pool.query(`select 
+export const getDetailBuy = async () => {
+  const [rows] = await pool.query(`select 
     d_b.id_detail, d_b.name, d_b.price, d_b.amount
     from buys as b
     inner join detail_buys as d_b
-    on b.id_buy = d_b.id_buys;`)
-    return rows
-
-}
+    on b.id_buy = d_b.id_buys;`);
+  return rows;
+};
 
 //Servicio que busca si ya existe un tipo de pago por el Id
 export const SearchPayId = async (id) => {
-
-    const [rows] = await pool.query('SELECT * FROM pay WHERE id_pay = ?', [id])
-    return rows.length
-    
-}
+  const [rows] = await pool.query("SELECT * FROM pay WHERE id_pay = ?", [id]);
+  return rows.length;
+};
 
 //Servicio que busca si ya existe un usuario por el Id
 export const SearchUserId = async (id) => {
-
-    const [rows] = await pool.query('SELECT * FROM users WHERE id_users = ?', [id])
-    return rows.length
-    
-}
+  const [rows] = await pool.query("SELECT * FROM users WHERE id_users = ?", [
+    id,
+  ]);
+  return rows.length;
+};
 
 //Servicio que devuelve el monto total a pagar en una compra
 export const Total_Buy = async (id) => {
-
-    const [rows] = await pool.query(`SELECT sum(p.price_product* c.amount_car) as total
+  const [rows] = await pool.query(
+    `SELECT sum(p.price_product* c.amount_car) as total
         FROM car as c inner join product as p
-        where id_users = ? and  c.id_product = p.id_product;`, [id])
-    return rows[0]
-    
-}
+        where id_users = ? and  c.id_product = p.id_product;`,
+    [id]
+  );
+  return rows[0];
+};
 
 //Servicio que registra una compra
 export const RegisterBuy = async (bu) => {
+  const total = bu.total;
+  const id_users = bu.id_users;
+  const id_pay = bu.id_pay;
 
-    const total = bu.total
-    const id_users = bu.id_users
-    const id_pay = bu.id_pay
-
-    const [rows] = await pool.query
-    ('INSERT INTO buys (total, date, id_users, id_pay) VALUES(?,CURDATE (),?,?)',
-    [total, id_users, id_pay])
-    const result = {
-        id: rows.insertId,
-        total,
-        id_users,
-        id_pay
-    }
-    return result
-}
+  const [rows] = await pool.query(
+    "INSERT INTO buys (total, date, id_users, id_pay) VALUES(?,CURDATE (),?,?)",
+    [total, id_users, id_pay]
+  );
+  const result = {
+    id: rows.insertId,
+    total,
+    id_users,
+    id_pay,
+  };
+  return result;
+};
 
 //Servicio que devuelve los datos de los productos que se estan comprando
 export const Data_Products = async (id) => {
-
-    const [rows] = await pool.query(`SELECT 
+  const [rows] = await pool.query(
+    `SELECT 
     p.id_product, p.name_product, p.price_product, c.amount_car
     FROM car as c inner join product as p
-    where id_users = ? and  c.id_product = p.id_product;`, [id])
-    return rows
-    
-}
+    where id_users = ? and  c.id_product = p.id_product;`,
+    [id]
+  );
+  return rows;
+};
 
 //Servicio que deletea el carrito de un usuario luego de la compra
 export const deletecar = async (id) => {
-
-    const [rows] = await pool.query(`delete from car where id_users =?;`, [id])
-    return rows
-    
-}
+  const [rows] = await pool.query(`delete from car where id_users =?;`, [id]);
+  return rows;
+};
 
 //Servicio que registra los detalles de una compra
 export const RegisterDetail_buy = async (produ_buy) => {
+  produ_buy.forEach(async (element) => {
+    const id_buys = element.id_buys;
+    const id_product = element.id_product;
+    const name = element.name;
+    const price = element.price;
+    const amount = element.amount;
 
-    produ_buy.forEach(async (element) => {
+    const [rows] = await pool.query(
+      "INSERT INTO detail_buys (amount, price, id_buys, id_product, name) VALUES(?,?,?,?,?)",
+      [amount, price, id_buys, id_product, name]
+    );
+    const result = {
+      id: rows.insertId,
+      amount,
+      price,
+      id_buys,
+      id_product,
+      name,
+    };
 
-        const id_buys = element.id_buys
-        const id_product = element.id_product
-        const name = element.name
-        const price = element.price
-        const amount = element.amount
+    const [rowss] = await pool.query(
+      `update product as p set amount_product = p.amount_product - ? where p.id_product = ?;`,
+      [amount, id_product]
+    );
 
-        const [rows] = await pool.query
-        ('INSERT INTO detail_buys (amount, price, id_buys, id_product, name) VALUES(?,?,?,?,?)',
-        [amount, price, id_buys, id_product, name])
-        const result = {
-            id: rows.insertId,
-            amount,
-            price,
-            id_buys,
-            id_product,
-            name
-        }
-
-        const [rowss] = await pool.query
-        (`update product as p set amount_product = p.amount_product - ? where p.id_product = ?;`,
-        [amount, id_product])
-
-        return result
-
-    })
-    
-}
+    return result;
+  });
+};
